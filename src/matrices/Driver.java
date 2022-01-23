c00887162e975727e96c62c20cac0781de8c9812
Raúl <raul@passporterapp.com>
2022-01-23 04:08:46 +0100

    FEAT(main): Adds logic to test trasponerMatriz matrix method.

--- src/matrices/Driver.java
+++ src/matrices/Driver.java
@@ -8,6 +8,7 @@ import matrices.Matriz;
 public class Driver {
 
     public static void main(String[] args) {
+        //Sumar matrices
         Matriz m1 = new Matriz(3, 4, true);
         System.out.println(m1);
         Matriz m2 = new Matriz(3, 4, true);
@@ -15,6 +16,15 @@ public class Driver {
         try {
             System.out.println(Matriz.sumarDosMatrices(m1, m2));
         } catch (DimensionesIncompatibles ex) {
+            Logger.getLogger(Driver.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        //Trasponer matriz
+        Matriz m3 = new Matriz(4, 3, true);
+        System.out.println(m3);
+        try {
+            System.out.println(Matriz.trasponerMatriz(m3));
+        } catch (Exception ex) {
             Logger.getLogger(Driver.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
